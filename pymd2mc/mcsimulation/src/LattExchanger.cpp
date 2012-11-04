904e9d81cc92314afc6ef3d9361fa89485cf1c07
Mateusz Lis <mateusz.lis@gmail.com>
2012-11-04 17:01:35 +0100

    [FIX] const removed, because polymorhpic call was badly resolved

--- pymd2mc/mcsimulation/src/LattExchanger.cpp
+++ pymd2mc/mcsimulation/src/LattExchanger.cpp
@@ -1,7 +1,7 @@
 #include "TriangularLattice.h"
 #include "LattExchanger.h"
 
-void LattExchanger::exchangeSites( lattIndex pos1, lattIndex pos2 ) const
+void LattExchanger::exchangeSites( lattIndex pos1, lattIndex pos2 )
 {
     // fast variable values exchange trick (pos1 != pos2)
 	mpLatt->getLattice()[pos1] ^= mpLatt->getLattice()[pos2];
