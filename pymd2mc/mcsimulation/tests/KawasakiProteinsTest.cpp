51659446f006c4fa25198c06e1e98a3c6c06d32e
Mateusz Lis <mateusz.lis@gmail.com>
2013-10-14 23:59:26 +0200

    [TEST] Test for new protein energy calc

--- pymd2mc/mcsimulation/tests/KawasakiProteinsTest.cpp
+++ pymd2mc/mcsimulation/tests/KawasakiProteinsTest.cpp
@@ -15,6 +15,7 @@
 
 #define protected public // to test hidden features
 #include "KawasakiProteins.h"
+#include "InteractionsTracker.h"
 
 // tests
 #include "testUtils.h"
@@ -23,13 +24,15 @@ using namespace testUtils;
 
 TEST( KawasakiProteins, calcEnergy )
 {
-    static const int LIPID_B_COUNT( 1 );
+    static const int LIPID_B_COUNT( 12 );
     static const int PROTEIN_COUNT( 1 );
-    std::tr1::shared_ptr< ProteinTriangularLattice > latt( new ProteinTriangularLattice( 9, 3, LIPID_B_COUNT, PROTEIN_COUNT, false ) );
-    printLatt( latt->getLattice(), 3, 3 );
+    std::tr1::shared_ptr< ProteinTriangularLattice > latt( new ProteinTriangularLattice( 64, 8, LIPID_B_COUNT, PROTEIN_COUNT, false ) );
+    printLatt( latt->getLattice(), 8, 8 );
     ProteinTriangularLattice* localPtr( latt.get() ); // constructor in line below requires raw ptr
     std::tr1::shared_ptr< KawasakiProteins > simulation( new KawasakiProteins( localPtr, 0, -400, 800 ) );
-    EXPECT_EQ( simulation->calcEnergy(), 5 * -400 + 5 * 800 );
+    EXPECT_EQ( simulation->calcEnergy(), 7 * -400 + 17 * 800 );
+    InteractionsTracker tracker( latt.get(), latt->mProteins, PROTEIN_COUNT );
+    testUtils::printLatt( tracker.mpProtInt.get(), 8, 8 );
 }
 
 #undef protected
